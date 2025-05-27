32cd5500ef61a6b7d455ae84b4a649ac94bb5136
Jesus789887wqe2 <jesus.prieto@plazacastilla.salesianas.org>
2025-05-27 10:23:30 +0200

    Add files via upload

---
+++ Primero.java
@@ -0,0 +1,20 @@
+package ejemplo;
+
+public class Primero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hola");
+		double x=7.4, y=8,z;
+		char inicial='B';
+		boolean becado=false;
+		z=x+y;
+		System.out.println("Mi inicial es:"+inicial);
+		System.out.println("La suma es:"+z);
+		System.out.println("Tengo beca:"+becado);
+		
+		z=x*y;
+		System.out.println("El producto es:"+z);
+	}
+	
+}
